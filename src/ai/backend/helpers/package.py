f5658310e8f90b2fa411eb82d2d45c478e27cec2
Joongi Kim <joongi@lablup.com>
2018-06-14 00:43:15 +0900

    helpers.package: Improve install() function
    * Flush stdout before/after running pip subprocess.
    * Ensure site.USER_SITE reside in sys.path so that the installed package
      becomes immediately importable.

--- src/ai/backend/helpers/package.py
+++ src/ai/backend/helpers/package.py
@@ -2,10 +2,8 @@ from collections import namedtuple
 from pathlib import Path
 import pkg_resources
 import site
-
 import subprocess
 import sys
-
 
 Package = namedtuple('Package', 'name version is_user')
 
@@ -16,7 +14,7 @@ def install(pkgname):
     The package is installed inside the user site directory.
     '''
 
-    user_path = Path(site.USER_SITE)
+    user_path = Path(site.USER_SITE).resolve()
     installed_pkgs = []
     for pkg in pkg_resources.working_set:
         pkg_path = Path(pkg.location).resolve()
@@ -28,4 +26,10 @@ def install(pkgname):
             print(f"'{pkg.name}' is already installed (version: {pkg.version}).")
             return
 
+    sys.stdout.flush()
     subprocess.call([sys.executable, '-m', 'pip', 'install', '--user', pkgname])
+    sys.stdout.flush()
+
+    # Ensure the user site directory to be in sys.path
+    if site.USER_SITE not in sys.path:
+        sys.path.insert(0, site.USER_SITE)
